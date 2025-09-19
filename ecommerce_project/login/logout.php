05271cbea68a2e97eab6ee9ba8a43ca5642fb960
Elorm Peggy Ameyibor <145340559+ae-peggy@users.noreply.github.com>
2025-09-19 20:19:52 +0000

    Update logout.php

--- ecommerce_project/login/logout.php
+++ ecommerce_project/login/logout.php
@@ -1,3 +1,13 @@
 <?php
+session_start();
 
-header('Location: ../login/login.php');
+// Destroy all session variables
+session_unset();
+
+// Destroy the session itself
+session_destroy();
+
+// Redirect to index page
+header("Location: ../index.php");
+exit();
+?>
